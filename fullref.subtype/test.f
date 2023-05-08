e505b0e2c1932ee235f7383dcd09f6503a08eb30
Shigma <shigma10826@gmail.com>
2023-05-09 01:26:03 +0800

    support backup instr counter class

--- fullref.subtype/test.f
+++ fullref.subtype/test.f
@@ -1,5 +1,3 @@
-/* Examples for testing */
-
 Counter = {
   get: Unit -> Nat,
   inc: Unit -> Unit
@@ -11,6 +9,19 @@ ResetCounter = {
   reset: Unit -> Unit
 };
 
+BackupCounter = {
+  get: Unit -> Nat,
+  inc: Unit -> Unit,
+  reset: Unit -> Unit,
+  backup: Unit -> Unit
+};
+
+SetCounter = {
+  get: Unit -> Nat,
+  set: Nat -> Unit,
+  inc: Unit -> Unit
+};
+
 DecCounter = {
   get: Unit -> Nat,
   inc: Unit -> Unit,
@@ -18,8 +29,26 @@ DecCounter = {
   dec: Unit -> Unit
 };
 
+InstrCounter = {
+  get: Unit -> Nat,
+  set: Nat -> Unit,
+  inc: Unit -> Unit,
+  accesses: Unit -> Nat
+};
+
 CounterRep = {
   x: Ref Nat
+};
+
+BackupCounterRep = {
+  x: Ref Nat,
+  b: Ref Nat
+};
+
+InstrCounterRep = {
+  x: Ref Nat,
+  a: Ref Nat,
+  b: Ref Nat
 };
 
 counterClass =
@@ -36,6 +65,24 @@ resetCounterClass =
       reset = lambda _: Unit. r.x := 1
     } as ResetCounter;
 
+backupCounterClass =
+  lambda r: BackupCounterRep.
+    let super = resetCounterClass r in {
+      get = super.get,
+      inc = super.inc,
+      reset = lambda _: Unit. (r.x := !(r.b)),
+      backup = lambda _: Unit. (r.b := !(r.x))
+    } as BackupCounter;
+
+setCounterClass =
+  lambda r: CounterRep.
+    lambda self: Unit -> SetCounter.
+      lambda _: Unit. {
+        get = lambda _: Unit. !(r.x),
+        set = lambda i: Nat. r.x := i,
+        inc = lambda _: Unit. (self unit).set (succ ((self unit).get unit))
+      } as SetCounter;
+
 decCounterClass =
   lambda r: CounterRep.
     let super = resetCounterClass r in {
@@ -44,3 +91,61 @@ decCounterClass =
       reset = super.reset,
       dec = lambda _: Unit. r.x := pred (!(r.x))
     } as DecCounter;
+
+instrCounterClass =
+  lambda r: InstrCounterRep.
+    lambda self: Unit -> InstrCounter.
+      lambda _: Unit.
+        let super = setCounterClass r self unit in {
+          get = lambda _: Unit. (r.a := succ (!(r.a)); super.get unit),
+          set = lambda i: Nat. (r.a := succ (!(r.a)); super.set i),
+          inc = super.inc,
+          accesses = lambda _: Unit. !(r.a)
+        } as InstrCounter;
+
+ResetInstrCounter = {
+  get: Unit -> Nat,
+  set: Nat -> Unit,
+  inc: Unit -> Unit,
+  reset: Unit -> Unit,
+  accesses: Unit -> Nat
+};
+
+resetInstrCounterClass =
+  lambda r: InstrCounterRep.
+    lambda self: Unit -> ResetInstrCounter.
+      lambda _: Unit.
+        let super = instrCounterClass r self unit in {
+          get = super.get,
+          set = super.set,
+          inc = super.inc,
+          reset = lambda _: Unit. (r.x := 0),
+          accesses = super.accesses
+        } as ResetInstrCounter;
+
+BackupInstrCounter = {
+  get: Unit -> Nat,
+  set: Nat -> Unit,
+  inc: Unit -> Unit,
+  reset: Unit -> Unit,
+  backup: Unit -> Unit,
+  accesses: Unit -> Nat
+};
+
+backupInstrCounterClass =
+  lambda r: InstrCounterRep.
+    lambda self: Unit -> ResetInstrCounter.
+      lambda _: Unit.
+        let super = resetInstrCounterClass r self unit in {
+          get = super.get,
+          set = super.set,
+          inc = super.inc,
+          reset = lambda _: Unit. (r.x := !(r.b)),
+          backup = lambda _: Unit. (r.b := !(r.x)),
+          accesses = super.accesses
+        } as BackupInstrCounter;
+
+newBackupInstrCounter =
+  lambda _: Unit.
+    let r = { x = ref 1, a = ref 0, b = ref 0 } in
+      fix (backupInstrCounterClass r) unit;
